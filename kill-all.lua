7b74a83b3cfb07f89d45bfb5b5385bf86b016925
Amukerd <146035837+amukerd@users.noreply.github.com>
2024-12-29 02:03:24 -0500

    Update kill-all.lua

--- kill-all.lua
+++ kill-all.lua
@@ -1,10 +1,9 @@
-local rs = game:GetService("ReplicatedStorage")
 local function r()
     for _, p in ipairs(game:GetService("Players"):GetPlayers()) do
         local pi = workspace:FindFirstChild(p.Name)
         if pi then
             local a={[1]=pi}
-            rs:WaitForChild("Modules"):WaitForChild("TypableNetwork"):WaitForChild("6"):InvokeServer(unpack(a))
+            game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("TypableNetwork"):WaitForChild("6"):InvokeServer(unpack(a))
         end
     end
 end
