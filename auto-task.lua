edbd416514649e89b0089eff54a3cf0db128fe74
Amukerd <146035837+amukerd@users.noreply.github.com>
2024-12-29 02:05:58 -0500

    Update auto-task.lua

--- auto-task.lua
+++ auto-task.lua
@@ -1,26 +1,25 @@
 local function z(parent)
-    local pl = game.Players.LocalPlayer
+    local pl=game.Players.LocalPlayer
     if not pl then return end
-
     for _, d in pairs(parent:GetDescendants()) do
         if d:IsA("ProximityPrompt") then
-            local a = d.Parent
+            local a=d.Parent
             if a and a:IsA("Attachment") then
-                local b = a.Parent
+                local b=a.Parent
                 if b and b:IsA("BasePart") then
-                    local l = b:FindFirstChild("LightContainer")
+                    local l=b:FindFirstChild("LightContainer")
                     if l then
-                        local p = l:FindFirstChildOfClass("PointLight")
+                        local p=l:FindFirstChildOfClass("PointLight")
                         if p then
-                            local rb = math.round(p.Brightness * 10^2) / 10^2
-                            local plc = p.Color
-                            local tc = Color3.fromRGB(137, 255, 111)
+                            local rb=math.round(p.Brightness*10^2)/10^2
+                            local plc=p.Color
+                            local tc=Color3.fromRGB(137,255,111)
 
-                            if rb == 0.28 and plc ~= tc then
+                            if rb==0.28 and plc~=tc then
                                 pl.Character:PivotTo(CFrame.new(b.Position))
                                 wait(0.5)
 
-                                local pp = d
+                                local pp=d
                                 game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.E, false, game)
                                 
                                 wait(pp.HoldDuration or 2)
@@ -35,5 +34,4 @@ local function z(parent)
         end
     end
 end
-
 z(workspace)
