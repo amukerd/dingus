4a3bb658879338603602f6330ac65098ae16a8e1
Amukerd <146035837+amukerd@users.noreply.github.com>
2024-12-29 02:14:45 -0500

    Update kill-all.lua

--- kill-all.lua
+++ kill-all.lua
@@ -1,5 +1,5 @@
 local function r()
-    for _, p in ipairs(game:GetService("Players"):GetPlayers()) do
+    for _,p in ipairs(game:GetService("Players"):GetPlayers()) do
         local pi=workspace:FindFirstChild(p.Name)
         if pi then
             local a={[1]=pi}
