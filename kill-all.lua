ad553b150b25bd5eaeeb8e1840698ba8b80e603a
Amukerd <146035837+amukerd@users.noreply.github.com>
2024-12-29 02:22:33 -0500

    Update kill-all.lua

--- kill-all.lua
+++ kill-all.lua
@@ -2,6 +2,5 @@ local function r()
 for _,p in ipairs(game:GetService("Players"):GetPlayers()) do
 local a={[1]=p.Name}
 game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("TypableNetwork"):WaitForChild("6"):InvokeServer(unpack(a))
-end
-end
+end end 
 r()
