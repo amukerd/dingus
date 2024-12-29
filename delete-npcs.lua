393016c704b8b072bba2ff447a4357620b811fbc
Amukerd <146035837+amukerd@users.noreply.github.com>
2024-12-29 12:38:16 -0500

    Update delete-npcs.lua

--- delete-npcs.lua
+++ delete-npcs.lua
@@ -1,6 +1,8 @@
 local function d()
-for _, c in ipairs(workspace:GetChildren()) do
-if c.Name=="PlayerCharacter" then
-c:Destroy()
-end end end
+    for _, c in ipairs(workspace:GetChildren()) do
+        if c.Name=="PlayerCharacter" then
+            c:Destroy()
+        end
+    end
+end
 d()
