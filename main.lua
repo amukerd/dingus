74814f8b99ec16f856711449487f497477e6abc5
Amukerd <146035837+amukerd@users.noreply.github.com>
2025-06-09 23:34:10 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -28,8 +28,8 @@ Tab:Button({
     Text = "Kill Players",
     Callback = function(self)
         for _, p in ipairs(game:GetService("Players"):GetPlayers()) do
-            local a = {[1] = workspace:FindFirstChild(p.Name)}
-            game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("TypableNetwork"):WaitForChild("6"):InvokeServer(unpack(a))
+            local args = {p.Character}
+            game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("TypedRemotes"):WaitForChild("22"):InvokeServer(unpack(args))
         end
     end,
     BackgroundTransparency = 0,
